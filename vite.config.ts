8c4c0e730fdd47135f3bf8dc48ac72f99a8166a0
Pierre Navarra <p.navarra@sona-architecture.com>
2026-05-18 10:24:12 +0200

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -9,26 +9,26 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg"],
       manifest: {
-        name: "FreeBloc",
-        short_name: "FreeBloc",
-        description: "Partage de blocs créés en salle",
-        theme_color: "#111827",
-        background_color: "#111827",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
+      name: "FreeBloc",
+      short_name: "FreeBloc",
+      description: "Partage de blocs créés en salle",
+      theme_color: "#000000",
+      background_color: "#000000",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        {
+          src: "/pwa-192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "/pwa-512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    }
     })
   ],
   server: {
