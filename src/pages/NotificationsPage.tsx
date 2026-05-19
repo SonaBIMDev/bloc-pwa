450a76da787471dd51fa4825fcb45cbddf4ebfa6
Pierre Navarra <p.navarra@sona-architecture.com>
2026-05-19 15:19:30 +0200

    new build

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, type User } from "firebase/auth";
 import { auth } from "../firebase";
 import { getNotificationsByUserId, markNotificationAsRead } from "../services/notificationsService";
